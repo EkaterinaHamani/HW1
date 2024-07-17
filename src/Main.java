81e8aea957f990a1678f19d5001aa55fd192bc60
HamaniEkaterina <kat.vee.cs@gmail.com>
2024-07-17 16:56:00 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main{
-    public static void main(String[] args) {}}
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+        circle.setFillColor("Green");
+        circle.setBorderColor("Grey");
+        circle.printCharacteristics();
+
+        Rectangle rectangle = new Rectangle(5,10);
+        rectangle.setFillColor("Red");
+        rectangle.setBorderColor("Grey");
+        rectangle.printCharacteristics();
+
+        Triangle triangle = new Triangle(5,6,8);
+        triangle.setFillColor("Yellow");
+        triangle.setBorderColor("Grey");
+        triangle.printCharacteristics();
+    }}
 
 
 
