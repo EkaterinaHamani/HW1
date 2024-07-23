b1fbc1873df4a9626d898d6b9f95989c0a9a7b46
EkaterinaHamani <158573807+EkaterinaHamani@users.noreply.github.com>
2024-07-23 15:03:54 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-String[] [] array = {{"0","0","0","0"},{"1","1","1","1"},{"2","2","2","2"},{"3","3","3","3"}};
-try{
-    int sum = ArrayConditions.sumAllArray(array);
-    System.out.println("Сумма массива равна: "+ sum);
-} catch (MyArraySizeException e) {
-    System.out.println("Исключение для размера: " + e.getMessage());
-} catch (MyArrayDataException e){
-    System.out.println("Исключение для формата. " + e.getMessage());
-}
 }}
 
 
