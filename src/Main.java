efd82359740b5b087e7940bcae77aafff2aaf968
HamaniEkaterina <kat.vee.cs@gmail.com>
2024-07-24 12:51:37 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,30 @@
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Задание 1");
+        String[] words = {"pizza", "spaghetti", "soup", "cake", "sandwich", "porridge", "omelette", "desert", "yogurt", "steak", "cake","cake","desert","desert","desert",};
+        Set<String> uniqueWords = WordConditions.findUniqueWords(words);
+        System.out.println("Уникальные слова:"+uniqueWords);
 
-String[] [] array = {{"0","0","0","0"},{"1","1","1","1"},{"2","2","2","2"},{"3","3","3","3"}};
-try{
-    int sum = ArrayConditions.sumAllArray(array);
-    System.out.println("Сумма массива равна: "+ sum);
-} catch (MyArraySizeException e) {
-    System.out.println("Исключение для размера: " + e.getMessage());
-} catch (MyArrayDataException e){
-    System.out.println("Исключение для формата. " + e.getMessage());
-}
-}}
+        Map<String,Integer> wordCases = WordConditions.countWordCases(words);
+        System.out.println("Повторение слов:"+wordCases);
+
+        System.out.println("Задание 2");
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Петров", "123");
+        phoneBook.add("Петров", "456");
+        phoneBook.add("Иванов", "789");
+        phoneBook.add("Сидоров", "000");
+
+
+        System.out.println(phoneBook.get("Петров"));
+        System.out.println(phoneBook.get("Иванов"));
+        System.out.println(phoneBook.get("Сидоров"));
+
+    }}
 
 
 
