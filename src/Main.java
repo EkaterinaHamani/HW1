a9073824331257156c19144174280bc769e924f9
HamaniEkaterina <kat.vee.cs@gmail.com>
2024-07-27 14:19:09 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
-System.out.println("Задание 1");
-String[] words = {"pizza", "spaghetti", "soup", "cake", "sandwich", "porridge", "omelette", "desert", "yogurt", "steak", "cake","cake","desert","desert","desert",};
-    Set<String> uniqueWords = WordConditions.findUniqueWords(words);
-    System.out.println("Уникальные слова:"+uniqueWords);
+        System.out.println("Задание 1");
+        String[] words = {"pizza", "spaghetti", "rice", "cake", "sandwich", "porridge", "omelette", "desert", "yogurt", "steak", "cake", "cake", "desert", "desert", "desert",};
+        Set<String> uniqueWords = WordConditions.findUniqueWords(words);
+        System.out.println("Уникальные слова:" + uniqueWords);
 
-        Map<String,Integer> wordCases = WordConditions.countWordCases(words);
-        System.out.println("Повторение слов:"+wordCases);
+        Map<String, Integer> wordCases = WordConditions.countWordCases(words);
+        System.out.println("Повторение слов:" + wordCases);
 
-System.out.println("Задание 2");
+        System.out.println("Задание 2");
         PhoneBook phoneBook = new PhoneBook();
         phoneBook.add("Петров", "123");
         phoneBook.add("Петров", "456");
@@ -23,4 +23,5 @@ System.out.println("Задание 2");
         System.out.println(phoneBook.get("Иванов"));
         System.out.println(phoneBook.get("Сидоров"));
 
-    }}
+    }
+}
--- src/WordConditions.java
+++ src/WordConditions.java
@@ -4,23 +4,22 @@ import java.util.Map;
 import java.util.Set;
 
 public class WordConditions {
-    public static Set<String> findUniqueWords(String[]words) {
+    public static Set<String> findUniqueWords(String[] words) {
         Set<String> uniqueWords = new HashSet<>();
-    for (String word : words) {
-        uniqueWords.add(word);
-    }
-    return uniqueWords;
+        for (String word : words) {
+            uniqueWords.add(word);
+        }
+        return uniqueWords;
     }
 
-    public static Map<String, Integer> countWordCases(String [] words){
+    public static Map<String, Integer> countWordCases(String[] words) {
         Map<String, Integer> wordCases = new HashMap<>();
         for (String word : words) {
-            if (wordCases.containsKey(word)){
+            if (wordCases.containsKey(word)) {
                 int count = wordCases.get(word);
-                wordCases.put(word,count+1);
-            }
-            else {
-                wordCases.put(word,1);
+                wordCases.put(word, count + 1);
+            } else {
+                wordCases.put(word, 1);
             }
         }
         return wordCases;
